6c7a610c7874db7c7d0e00ece57eccdd106ba9b4
Kirill <akulenko818@gmail.com>
2023-03-10 10:17:40 +0300

    start

--- PlanetsV2/ViewControllers/DevViewController.swift
+++ PlanetsV2/ViewControllers/DevViewController.swift
@@ -11,7 +11,7 @@ class DevViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .systemYellow
         // Do any additional setup after loading the view.
     }
     
