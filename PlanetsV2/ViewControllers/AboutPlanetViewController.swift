76161c6b719317cab610ab15f262bacc24376cf7
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-03-11 18:07:39 +0300

    Код для отображения информации об отдельной планете.

--- PlanetsV2/ViewControllers/AboutPlanetViewController.swift
+++ PlanetsV2/ViewControllers/AboutPlanetViewController.swift
@@ -8,22 +8,25 @@
 import UIKit
 
 class AboutPlanetViewController: UIViewController {
-
+    
+    @IBOutlet weak var planetName: UILabel!
+    @IBOutlet weak var planetDescription: UILabel!
+    
+    var planet: Planet!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //planetImage.image = UIImage(named: planet.englishName)
+        planetName.text = planet.englishName
+        planetDescription.text = planet.description
     }
-    
-
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
 }
