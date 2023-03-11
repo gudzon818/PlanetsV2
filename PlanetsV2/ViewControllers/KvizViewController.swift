af9938ae8f400b8b29fa6c4cfdc57ed128ad1d88
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-03-11 09:53:03 +0300

    Работа с кодом для квиза.

--- PlanetsV2/ViewControllers/KvizViewController.swift
+++ PlanetsV2/ViewControllers/KvizViewController.swift
@@ -9,13 +9,29 @@ import UIKit
 
 class KvizViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet var answersButton: [UIButton]!
+    
+    private let questions = Question.getQuestion()
+    private let questionIndex = 0
+    private var currentAnswers: [Answer] {
+        questions[questionIndex].answers
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
         // Do any additional setup after loading the view.
     }
     
-
+    
+    @IBAction func answerButtonTapped(_ sender: UIButton) {
+        guard let buttonIndex = answersButton.firstIndex(of: sender) else { return }
+        
+        let currentAnswer = currentAnswers[buttonIndex]
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -26,4 +42,27 @@ class KvizViewController: UIViewController {
     }
     */
 
+}
+
+// MARK: - Set a private methods
+
+extension KvizViewController {
+    func updateUI() {
+        let currentQuestion = questions[questionIndex]
+        questionLabel.text = currentQuestion.title
+        titleLabel.text = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        
+        showCurrentAnswers()
+    }
+    
+    func showCurrentAnswers() {
+        showButtonsAnswers(with: currentAnswers)
+    }
+    
+    func showButtonsAnswers(with answers: [Answer]) {
+
+        for (button, answer) in zip(answersButton, answers) {
+            button.setTitle(answer.title, for: .normal)
+        }
+    }
 }
