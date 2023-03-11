866831568ff2e3ae44706365e5411f01fbe7a2d7
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-03-11 18:06:24 +0300

    Просмотр.

--- PlanetsV2/ViewControllers/StartViewController.swift
+++ PlanetsV2/ViewControllers/StartViewController.swift
@@ -15,16 +15,16 @@ class StartViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        guard let tabBarVC = segue.destination as? UITabBarController else { return }
-//        guard let viewControllers = tabBarVC.viewControllers else { return }
-//        viewControllers.forEach { ViewController in
-//            if let planetTableVC = ViewController as? PlanetsTableViewController {
-//                planetTableVC.planets = planets
-//            } else if let kvizVC = ViewController as? KvizViewController {
-//                kvizVC.questions = question
-//
-//            }
-//        }
-//    }
+    //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    //        guard let tabBarVC = segue.destination as? UITabBarController else { return }
+    //        guard let viewControllers = tabBarVC.viewControllers else { return }
+    //        viewControllers.forEach { ViewController in
+    //            if let planetTableVC = ViewController as? PlanetsTableViewController {
+    //                planetTableVC.planets = planets
+    //            } else if let kvizVC = ViewController as? KvizViewController {
+    //                kvizVC.questions = question
+    //
+    //            }
+    //        }
+    //    }
 }
