674b494f6d12a42319e9bc791ca5c3c0dbd6d40d
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-03-11 18:08:01 +0300

    Проработка кода

--- PlanetsV2/ViewControllers/FinalResultViewController.swift
+++ PlanetsV2/ViewControllers/FinalResultViewController.swift
@@ -8,24 +8,36 @@
 import UIKit
 
 class FinalResultViewController: UIViewController {
-
+    
     @IBOutlet weak var showFinalResult: UILabel!
     
+    //var counter = 0
+    //var questions: [Question]!
     var answers: [Answer] = []
+    
+    //    private func getAnswerNumber() {
+    //
+    //        if answers.count == numberOfCorrectAnswers.count {
+    //            showFinalResult.text =  "Вы ответили правильно на \(numberOfCorrectAnswers.count) из \(answers.count) вопросов"
+    //        }
+    //    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //getAnswerNumber()
+        //showFinalResult.text =  "Вы ответили правильно на \(counter) вопросов."
+        //print(answers)
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- PlanetsV2/ViewControllers/KvizViewController.swift
+++ PlanetsV2/ViewControllers/KvizViewController.swift
@@ -8,13 +8,14 @@
 import UIKit
 
 class KvizViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet var answersButton: [UIButton]!
     
     private let questions = Question.getQuestion()
     private var questionIndex = 0
+    //private var count = 0
     private var givenAnswers: [Answer] = []
     private var currentAnswers: [Answer] {
         questions[questionIndex].answers
@@ -25,31 +26,50 @@ class KvizViewController: UIViewController {
         updateUI()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let finalResultVC = segue.destination as? FinalResultViewController else { return }
+        //finalResultVC.counter = count
+        finalResultVC.answers = givenAnswers
+    }
+    
+    // Set up button action after button tapped
     
     @IBAction func answerButtonTapped(_ sender: UIButton) {
         guard let buttonIndex = answersButton.firstIndex(of: sender) else { return }
         
         let currentAnswer = currentAnswers[buttonIndex]
         givenAnswers.append(currentAnswer)
+        print(currentAnswers[buttonIndex].status)
+        
+        //        if currentAnswers[buttonIndex].status == currentAnswer.status {
+        //            count += 1
+        //            goToNextStep()
+        //        } else {
+        //            print("Wrong answer!")
+        //            goToNextStep()
+        //        }
         
         goToNextStep()
     }
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
 
 // MARK: - Set a private methods
 
 extension KvizViewController {
+    
+    // updateUI for next question
+    
     func updateUI() {
         let currentQuestion = questions[questionIndex]
         questionLabel.text = currentQuestion.title
@@ -58,16 +78,22 @@ extension KvizViewController {
         showCurrentAnswers()
     }
     
+    // Set up method current question
+    
     func showCurrentAnswers() {
         showButtonsAnswers(with: currentAnswers)
     }
     
+    // Method to represent answers
+    
     func showButtonsAnswers(with answers: [Answer]) {
-
+        
         for (button, answer) in zip(answersButton, answers) {
             button.setTitle(answer.title, for: .normal)
         }
     }
+    
+    // Next question and pass result to FinalResultViewController
     
     func goToNextStep() {
         questionIndex += 1
