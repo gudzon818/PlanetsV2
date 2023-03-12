7c019648bfb1d91101c25fe160a2ab78016f4800
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-03-12 22:09:21 +0300

    Корректировка.

--- PlanetsV2/ViewControllers/AboutPlanetViewController.swift
+++ PlanetsV2/ViewControllers/AboutPlanetViewController.swift
@@ -20,13 +20,4 @@ class AboutPlanetViewController: UIViewController {
         planetName.text = planet.englishName
         planetDescription.text = planet.description
     }
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
 }
