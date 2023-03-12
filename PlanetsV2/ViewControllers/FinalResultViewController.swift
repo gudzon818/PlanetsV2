6db81b13bb11e2f2eb6eb4aa09b40712d1abd496
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-03-12 21:59:02 +0300

    Код для вывода результата на экран.

--- PlanetsV2/ViewControllers/FinalResultViewController.swift
+++ PlanetsV2/ViewControllers/FinalResultViewController.swift
@@ -10,34 +10,22 @@ import UIKit
 class FinalResultViewController: UIViewController {
     
     @IBOutlet weak var showFinalResult: UILabel!
-    
-    //var counter = 0
-    //var questions: [Question]!
+
+    private var count = 0
     var answers: [Answer] = []
-    
-    //    private func getAnswerNumber() {
-    //
-    //        if answers.count == numberOfCorrectAnswers.count {
-    //            showFinalResult.text =  "Вы ответили правильно на \(numberOfCorrectAnswers.count) из \(answers.count) вопросов"
-    //        }
-    //    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //getAnswerNumber()
-        //showFinalResult.text =  "Вы ответили правильно на \(counter) вопросов."
-        //print(answers)
+        calculateResult()
+        showFinalResult.text =  "Ваш результат равен - \(count)."
     }
     
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+    private func calculateResult() {
+        for answer in answers {
+            if answer.status == true {
+                count += 1
+            }
+            continue
+        }
+    }
 }
