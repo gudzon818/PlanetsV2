64d86e07a7494e955b1189810deb23957fe33a40
Mikhail German <mikhail.german@icloud.com>
2023-03-14 11:08:17 +0500

    Добавлен экран о разработчиках

--- PlanetsV2/ViewControllers/StartViewController.swift
+++ PlanetsV2/ViewControllers/StartViewController.swift
@@ -25,7 +25,6 @@ class StartViewController: UIViewController {
     
     @objc func planetTapped() {
         let scale: CGFloat = 1.5
-        
         UIView.animate(withDuration: 0.3, delay: 0, options: [.curveEaseInOut], animations: {
             self.planetUiImageView.transform = CGAffineTransform(scaleX: scale, y: scale)
         }) { (finished) in
@@ -34,24 +33,7 @@ class StartViewController: UIViewController {
             }, completion: nil)
         }
     }
-    
-    
-   
 
-    
-    //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    //        guard let tabBarVC = segue.destination as? UITabBarController else { return }
-    //        guard let viewControllers = tabBarVC.viewControllers else { return }
-    //        viewControllers.forEach { ViewController in
-    //            if let planetTableVC = ViewController as? PlanetsTableViewController {
-    //                planetTableVC.planets = planets
-    //            } else if let kvizVC = ViewController as? KvizViewController {
-    //                kvizVC.questions = question
-    //
-    //            }
-    //        }
-    //    }
-    
     
     //: MARK UIButton and UIImageView settings
     private func buttonSettings() {
@@ -68,22 +50,4 @@ class StartViewController: UIViewController {
         planetUiImageView.layer.add(rotationAnimation, forKey: "rotationAnimation")
     }
 }
-    
-        
-
-//        infoButton.layer.cornerRadius = 10
-//
-//
-//        let gradientLayer = CAGradientLayer()
-//        gradientLayer.frame = infoButton.bounds
-//        gradientLayer.colors = [
-//            UIColor(red: 13/255, green: 35/255, blue: 58/255, alpha: 1),
-//            UIColor(red: 43/255, green: 1/255, blue: 61/255, alpha: 1)
-//        ]
-//        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
-//        gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.5)
-//
-//        infoButton.layer.insertSublayer(gradientLayer, at: 0)
-//        infoButton.layer.cornerRadius = 10
-//
 
